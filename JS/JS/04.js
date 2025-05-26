479b5a8694f11f303c0b16fd322e182fd287225f
jeonjeongbin <01jjb@naver.com>
2025-05-26 12:10:15 +0900

    커밋

---
+++ JS/JS/04.js
@@ -0,0 +1,7 @@
+// 함수를 정의 
+function over(obj) {      //over 함수 : 매개변수로 obj
+    obj.src = './img/banana.png';
+}
+function out(obj) {      //out 함수 
+    obj.src = './img/apple.png';
+}
---
+++ JS/JS/06.js
@@ -0,0 +1,48 @@
+// 나의 정보
+function my_info() {
+  const name    = "전정빈";
+  const age     = 25;
+  const phone   = "010-1234-5678";
+  const weight  = 70.5;
+
+  alert(
+    `내 정보
+이름       : ${name}
+나이       : ${age}세
+전화번호   : ${phone}
+몸무게     : ${weight}kg`
+  );
+}
+
+// 형제의 정보
+function brother_info() {
+  const name    = "전길동";
+  const age     = 27;
+  const phone   = "010-8765-4321";
+  const weight  = 68.2;
+
+  alert(
+    `형제 정보
+이름       : ${name}
+나이       : ${age}세
+전화번호   : ${phone}
+몸무게     : ${weight}kg`
+  );
+}
+
+// 부모의 정보
+function parent_info() {
+  const name    = "홍철수";
+  const age     = 55;
+  const phone   = "02-123-4567";
+  const weight  = 72.3;
+
+  alert(
+    `부모 정보
+이름       : ${name}
+나이       : ${age}세
+전화번호   : ${phone}
+몸무게     : ${weight}kg`
+  );
+}
+

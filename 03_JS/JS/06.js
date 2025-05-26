dc9e7b35e66dae5fb73344bbf2704395f11e2fa0
jeonjeongbin <01jjb@naver.com>
2025-05-26 16:58:16 +0900

    커밋

--- 03_JS/JS/06.js
+++ 03_JS/JS/06.js
@@ -1,9 +1,9 @@
 // 나의 정보
 function my_info() {
-  const name    = "전정빈";
-  const age     = 25;
-  const phone   = "010-1234-5678";
-  const weight  = 70.5;
+  const name = "전정빈";
+  const age = 25;
+  const phone = "010-1234-5678";
+  const weight = 70.5;
 
   alert(
     `내 정보
@@ -16,10 +16,10 @@ function my_info() {
 
 // 형제의 정보
 function brother_info() {
-  const name    = "전길동";
-  const age     = 27;
-  const phone   = "010-8765-4321";
-  const weight  = 68.2;
+  const name = "전길동";
+  const age = 27;
+  const phone = "010-8765-4321";
+  const weight = 68.2;
 
   alert(
     `형제 정보
@@ -32,10 +32,10 @@ function brother_info() {
 
 // 부모의 정보
 function parent_info() {
-  const name    = "홍철수";
-  const age     = 55;
-  const phone   = "02-123-4567";
-  const weight  = 72.3;
+  const name = "홍철수";
+  const age = 55;
+  const phone = "02-123-4567";
+  const weight = 72.3;
 
   alert(
     `부모 정보
